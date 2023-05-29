4284bec5b706a2d60ccafcca5824ac8c81365af0
juleimisf <juleimis1314@gmail.com>
2023-05-28 23:49:38 -0400

    Add new test for compose

--- app/src/androidTest/java/com/example/composetutorial/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/composetutorial/ExampleInstrumentedTest.kt
@@ -1,24 +1,39 @@
 package com.example.composetutorial
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.os.Bundle
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
-import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class PlantDetailFragmentTest {
+
+    @Rule
+    @JvmField
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Before
+    fun jumpToPlantDetailFragment() {
+        //populateDatabase()
+
+        composeTestRule.activityRule.scenario.onActivity { gardenActivity ->
+           // activity = gardenActivity
+
+            val bundle = Bundle().apply { putString("plantId", "malus-pumila") }
+           // findNavController(activity, R.id.nav_host).navigate(R.id.plant_detail_fragment, bundle)
+        }
+    }
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.composetutorial", appContext.packageName)
+    fun testPlantName() {
+        composeTestRule.onNodeWithText("Apple").assertIsDisplayed()
     }
 }
