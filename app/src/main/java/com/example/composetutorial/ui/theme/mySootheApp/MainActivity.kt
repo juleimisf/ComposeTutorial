5947794dc89292b1c0a575d55f17a06b1d014da3
juleimisf <juleimis1314@gmail.com>
2023-06-04 22:57:23 -0400

    Add new view favorite and body content

--- app/src/main/java/com/example/composetutorial/ui/theme/mySootheApp/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/ui/theme/mySootheApp/MainActivity.kt
@@ -3,20 +3,24 @@ package com.example.composetutorial.ui.theme.mySootheApp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.materialIcon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.material.Text
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
+import com.example.composetutorial.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,10 +58,70 @@ fun SearchBar(
     )
 }
 
-@Preview
+@Composable
+fun AlignYourBodyElement(modifier: Modifier = Modifier, data: DataBody) {
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            painter = data.image, contentDescription = null,
+            contentScale = ContentScale.Crop, modifier = Modifier
+                .size(44.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = data.name,
+            style = MaterialTheme.typography.h6,
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp)
+        )
+    }
+}
+
+data class DataBody(
+    val image: Painter,
+    val name: String
+)
+
+@Composable
+fun FavoriteCollectionCard(modifier: Modifier = Modifier, data: DataBody) {
+    Surface(shape = MaterialTheme.shapes.small, modifier = modifier) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.width(192.dp)
+        ) {
+            Image(
+                painter = data.image,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(56.dp)
+            )
+            Text(
+                text = data.name
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
 @Composable
 private fun SearchBarPreview() {
     ComposeTutorialTheme {
         SearchBar()
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Composable
+private fun AlignYourBodyElementPreview() {
+    ComposeTutorialTheme {
+        val data = DataBody(image = painterResource(id = R.drawable.ic_clock), name = "Nombre")
+        AlignYourBodyElement(modifier = Modifier.padding(8.dp), data = data)
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Composable
+private fun FavoriteCollectionCardPreview() {
+    ComposeTutorialTheme {
+        val data = DataBody(image = painterResource(id = R.drawable.ic_clock), name = "Nombre")
+        FavoriteCollectionCard(modifier = Modifier.padding(8.dp), data = data)
     }
 }
