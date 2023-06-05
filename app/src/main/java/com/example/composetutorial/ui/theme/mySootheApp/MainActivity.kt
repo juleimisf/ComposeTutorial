09b90c6aabe77c4db7f567d1e9d3629890d8e6fd
juleimisf <juleimis1314@gmail.com>
2023-06-04 22:22:27 -0400

    Add new view for search and add mofifiers

--- app/src/main/java/com/example/composetutorial/ui/theme/mySootheApp/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/ui/theme/mySootheApp/MainActivity.kt
@@ -1,13 +1,63 @@
 package com.example.composetutorial.ui.theme.mySootheApp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.composetutorial.R
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.material.Text
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            ComposeTutorialTheme() {
+                SearchBar()
+            }
+        }
+    }
+}
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface
+        ),
+        placeholder = {
+            Text("Search")
+        },
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp)
+    )
+}
+
+@Preview
+@Composable
+private fun SearchBarPreview() {
+    ComposeTutorialTheme {
+        SearchBar()
     }
 }
